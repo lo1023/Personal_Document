d2a5d73a293a83b52f425a2494371ad70a9cb817
lo1023 <lo1019@naver.com>
2021-01-13 22:04:05 +0900

    Subject :  파이썬 숫자처리함수까지 공부
    To do : 파이썬은 존나게 쉽다.. 자료형이 죄다 auto인가봄

--- Python/HelloPython.py
+++ Python/HelloPython.py
@@ -1,1 +1,22 @@
 print("Hello Phthon")
+
+print(5+5)
+
+animal = 1
+isage = 3>5
+
+print("파이썬은 변수형이 죄다 auto냐?" + str(animal))
+
+#한줄 주석
+'''
+여러줄 주
+'''
+
+print(2**3) #2^3
+print(6//3) #2
+print(not(1)) # !
+print( 3 > 0 and 2 < 1) # &
+print(3 > 0 or 1 < 2) # |
+
+from math import *  # #include
+print(floor(4.99)) # 내림
